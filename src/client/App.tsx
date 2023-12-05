f91f75d74de4afd83dff4eba22b99447173528b7
Luke Przkwas <luke@covalence.io>
2023-12-05 14:27:28 -0600

    completing lab step 2

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,18 +3,29 @@ import React, { useState, useEffect } from 'react';
 interface AppProps {}
 
 const App = (props: AppProps) => {
-	const [data, setData] = useState('');
+	const [data1, setData1] = useState('');
+	const [data2, setData2] = useState('');
 
 	useEffect(() => {
+		let data1Temp = '';
 		fetch('http://localhost:3000/api/hello')
 			.then(res => res.json())
-			.then(data => setData(data.message))
+			.then(data => {
+				data1Temp = data.message;
+				return fetch('http://localhost:3000/api/goodbye');
+			})
+			.then(res => res.json())
+			.then(data => {
+				setData1(data1Temp);
+				setData2(data.message);
+			})
 			.catch(e => console.log('[fetch erorr]', e));
 	}, []);
 
 	return (
 		<div className="mx-auto mt-5 w-25">
-			<div className="alert alert-info text-center">Hello {data}</div>
+			<div className="alert alert-info text-center">Hello {data1}</div>
+			<div className="alert alert-info text-center">Goodbye {data2}</div>
 		</div>
 	);
 };
